9c9ed22dfd2bf3c0455824b55cbc238e1df934bd
yorutoitsuki <thouxcv@gmail.com>
2022-08-19 15:07:52 +0900

    done

--- FormEvent/FormEvent/Program.cs
+++ FormEvent/FormEvent/Program.cs
@@ -4,6 +4,22 @@ namespace FormEvent
 {
     public class FormEvent
     {
+        public FormEvent()
+        {
+            this.Click += new EventHandler(ClickEvent);
+        }
 
+        private void ClickEvent (object sender, EventArgs e)
+        {
+            MessageBox.Show("마우스 클릭 이벤트를 처리합니다");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Application.Run(new FormEvent());
+        }
     }
 }
