d21cf3c0671f61a27a6292c3f40f494ae4992d35
yorutoitsuki <thouxcv@gmail.com>
2022-08-19 09:56:33 +0900

    delegate Start

---
+++ Interface2/Interface2/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Interface
+{
+    public interface ICamera
+    {
+        void Picture();
+        void Video();
+    }
+
+    public interface ICallPhone
+    {
+        void Call();
+    }
+
+    public interface INetwork
+    {
+        void Wifi();
+    }
+
+    public class SmartPhone : ICamera, ICallPhone, INetwork
+    {
+        public void Picture()
+        {
+            Console.WriteLine("Take Picture");
+        }
+
+        public void Video()
+        {
+            Console.WriteLine("Take Video");
+        }
+
+        public void Call()
+        {
+            Console.WriteLine("Get Call");
+        }
+
+        public void Wifi()
+        {
+            Console.WriteLine("Connect Wifi");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
