c835d5f79f1dd770313b24021905dd5c6f5b7abe
yorutoitsuki <thouxcv@gmail.com>
2022-08-19 10:38:03 +0900

    delegate

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿namespace Delegate
+{
+    class Program
+    {
+        delegate int CalcDelegate(int x, int y);
+
+        static int Plus(int x, int y)
+        {
+            return x + y;
+        }
+        static int Minus(int x, int y)
+        {
+            return x - y;
+        }
+
+        static void Main()
+        {
+            CalcDelegate del1 = new CalcDelegate(Plus);
+            int result = del1(20, 10);
+            Console.WriteLine("result = " + result);
+
+            CalcDelegate del2 = new CalcDelegate(Minus);
+            result = del2(20, 10);
+            Console.WriteLine("result = " + result);
+        }
+    }
+}
