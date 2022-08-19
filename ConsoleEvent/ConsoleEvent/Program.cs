f120d8f2d75efa753317316c5b26f5013faffacf
yorutoitsuki <thouxcv@gmail.com>
2022-08-19 14:01:25 +0900

    event_work

---
+++ ConsoleEvent/ConsoleEvent/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleEvent
+{
+    class Program
+    {
+        public delegate void ButtonEvent();
+        public static event ButtonEvent evtClick;
+        public static void onClick()
+        {
+            evtClick();
+        }
+        public static void MyHandler()
+        {
+            Console.WriteLine("버튼 클릭 처리기 수행");
+        }
+        static void Main()
+        {
+            evtClick += new ButtonEvent(MyHandler);
+            onClick();
+        }
+    }
+}
