5347ff496f58df43584a8ddd1da82548c68860ff
yorutoitsuki <thouxcv@gmail.com>
2022-08-22 10:45:18 +0900

    maybe

---
+++ C_sharp_ch4/C_sharp_ch4/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Currency
+{
+    decimal money;
+    public decimal Money { get { return money; } }
+    public Currency(decimal money)
+    {
+        this.money = money;
+    }
+}
+
+public class Won : Currency
+{
+    public Won(decimal money) : base(money) { }
+    public override string ToString()
+    {
+        return Money + " Won";
+    }
+}
+
+public class Dollar : Currency
+{
+    public Dollar(decimal money) : base (money) { }
+    public override string ToString()
+    {
+        return Money + " Dollar";
+    }
+}
+public class Yen : Currency
+{
+    public Yen(decimal money) : base(money) { }
+    public override string ToString()
+    {
+        return Money + " Yen";
+    }
+    static public implicit operator Won(Yen yen)
+    {
+        return new Won(yen.Money * 13m);
+    }
+}
+
+namespace C_sharp_ch4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Won won = new Won(1000);
+            Dollar dollar = new Dollar(1);
+            Yen yen = new Yen(13);
+
+            
+        }
+    }
+}
