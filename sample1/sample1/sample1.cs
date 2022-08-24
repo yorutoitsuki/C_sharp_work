eb276b7a9e6d823e815da2cb798c9ac5fddf7f39
yorutoitsuki <thouxcv@gmail.com>
2022-08-24 09:13:51 +0900

    150p

---
+++ sample1/sample1/sample1.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+class Sample2
+{
+    public static void Main()
+    {
+        Form fm = new Form();
+        fm.Text = "Welcom to C#";
+
+        Label lb = new Label();
+        lb.Width = 150;
+        lb.Text = "Start C#";
+        lb.Parent = fm;
+
+        PictureBox pb = new PictureBox();
+        pb.Image = Image.FromFile("C:\\Users\\yn-17\\2022_영남_이승기\\HTML\\Image\\flower.jpg");
+        pb.Parent = fm;
+
+        Application.Run(fm);
+    }
+}
