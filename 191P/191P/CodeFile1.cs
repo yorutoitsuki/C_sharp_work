39693c50d9504e0aeae4ff35218ccd79c03d894d
yorutoitsuki <thouxcv@gmail.com>
2022-08-25 14:08:25 +0900

    doNot_btn_Key_event_togather

---
+++ 191P/191P/CodeFile1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+/*
+ * 
+ * 
+ * 
+ * 버튼 이벤트가 키보드 이벤트를 모두 흡수해버림,
+ * 버튼 이벤트와 키보드 이벤트를 따로 구현해야함
+ * 
+ * 
+ * 
+ */
+class Sample2 : Form
+{
+    private Label lb1, lb2;
+
+    public static void Main()
+    {
+        Application.Run(new Sample2());
+
+    }
+    public Sample2()
+    {
+        this.Text = "샘플";
+        this.Width = 250; this.Height = 300;
+
+        lb1 = new Label();
+        lb1.Text = "어서오세요";
+        lb1.Width = 150;
+
+        lb2 = new Label();
+        lb2.Text = "방향키를 입력해주세요";
+        lb2.Top = lb1.Bottom;
+        lb1.Parent = this;
+        lb2.Parent = this;
+        this.KeyDown += new KeyEventHandler(fm_KeyDown);
+        lb1.MouseEnter += new EventHandler(fm_MouseEnter);
+        lb1.MouseLeave += new EventHandler(fm_MouseLeave);
+
+
+
+    }
+    public void fm_MouseEnter(Object sender, EventArgs e)
+    {
+        lb1.Text = "안녕하세요";
+    }
+    public void fm_MouseLeave(Object sender, EventArgs e)
+    {
+        lb1.Text = "안녕히 가세요";
+    }
+    public void fm_KeyDown(Object sender, KeyEventArgs e)
+    {
+        lb1.Text = "Error";
+        String str;
+        if (e.KeyCode == Keys.Up)
+        {
+            str = "위쪽";
+        }
+        else if (e.KeyCode == Keys.Down)
+        {
+            str = "아래쪽";
+        }
+        else if (e.KeyCode == Keys.Left)
+        {
+            str = "왼쪽";
+        }
+        else if (e.KeyCode == Keys.Right)
+        {
+            str = "오른쪽";
+        }
+        else
+        {
+            str = "다른키";
+        }
+        lb2.Text = str;
+    }
+}
---
+++ 195P/195P/CodeFile1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Forms;
+
+class Sample4 : Form
+{
+    private Label lb1, lb2,lb3;
+    public static void Main()
+    {
+        Application.Run(new Sample4());
+    }
+
+    public Sample4()
+    {
+        this.Text = "샘플";
+        this.Width = 250; this.Height = 100;
+
+        lb1 = new Label();
+        lb1.Text = "방향키로 선택하세요";
+        lb1.Width = this.Width;
+
+        lb2 = new Label();
+        lb2.Top = lb1.Bottom;
+
+        lb3 = new Label();
+        lb3.Text = "testing";
+        lb3.Top = lb2.Bottom;
+
+        lb1.Parent = this;
+        lb2.Parent = this;
+        lb3.Parent = this;
+        this.KeyDown += new KeyEventHandler(fm_KeyDown);
+        lb3.MouseEnter += new EventHandler(mouseEnter);
+        lb3.MouseLeave += new EventHandler(mouseLeave);
+    }
+
+    public void mouseEnter(object sender, EventArgs e)
+    {
+        lb3.Text = "test in";
+    }
+    public void mouseLeave(object sender, EventArgs e)
+    {
+        lb3.Text = "test out";
+    }
+
+
+    public void fm_KeyDown(object sender, KeyEventArgs e)
+    {
+        String str;
+        if(e.KeyCode == Keys.Up)
+        {
+            str = "위쪽";
+        }
+        else if(e.KeyCode == Keys.Down)
+        {
+            str = "아래쪽";
+        }
+        else if(e.KeyCode == Keys.Left)
+        {
+            str = "왼쪽";
+        }
+        else if(e.KeyCode == Keys.Right)
+        {
+            str = "오른쪽";
+        }
+        else
+        {
+            str = "다른 키";
+        }
+        lb2.Text = str;
+    }
+
+}
