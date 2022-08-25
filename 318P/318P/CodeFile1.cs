f42016784b302bea8908a407b981262f43c0ba10
yorutoitsuki <thouxcv@gmail.com>
2022-08-25 16:29:48 +0900

    322P

---
+++ 318P/318P/CodeFile1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+class Sample1 : Form
+{
+    private Button bt;
+    private Label[] lb = new Label[3];
+
+    [STAThread]
+    public static void Main()
+    {
+        Application.Run(new Sample1());
+    }
+    public Sample1()
+    {
+        this.Text = "샘플";
+        this.Width = 300; this.Height = 200;
+
+        for(int i= 0; i < lb.Length; i++)
+        {
+            lb[i] = new Label();
+            lb[i].Top = i * lb[0].Height;
+            lb[i].Width = 300;
+        }
+
+        bt = new Button();
+        bt.Text = "표시";
+        bt.Dock = DockStyle.Bottom;
+
+        bt.Parent = this;
+
+        for(int i = 0; i < lb.Length; i++)
+        {
+            lb[i].Parent = this;
+        }
+        bt.Click += new EventHandler(bt_Click);
+    }
+    public void bt_Click(Object sender, EventArgs e)
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+        if(ofd.ShowDialog() == DialogResult.OK)
+        {
+            FileInfo fi = new FileInfo(ofd.FileName);
+            lb[0].Text = "파일명은 " + ofd.FileName + "입니다";
+            lb[1].Text = "절대 경로는 " + Path.GetFullPath(ofd.FileName) + "입니다";
+            lb[2].Text = "크기는 " + Convert.ToString(fi.Length) + "입니다";
+        }
+    }
+}
---
+++ 322P/322P/CodeFile1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+class Sample2 : Form
+{
+    private TextBox tb;
+    private Button bt1, bt2;
+    private FlowLayoutPanel flp;
+
+    [STAThread]
+    public static void Main()
+    {
+        Application.Run(new Sample2());
+    }
+    public Sample2()
+    {
+        this.Text = "샘플";
+
+        tb = new TextBox();
+        tb.Multiline = true;
+        tb.Width = this.Width; tb.Height = this.Height - 100;
+        tb.Dock = DockStyle.Top;
+
+        bt1 = new Button();
+        bt2 = new Button();
+        bt1.Text = "읽어 들이기";
+        bt2.Text = "저장";
+
+        flp = new FlowLayoutPanel();
+        flp.Dock = DockStyle.Bottom;
+
+        bt1.Parent = flp;
+        bt2.Parent = flp;
+        flp.Parent = this;
+        tb.Parent = this;
+
+        bt1.Click += new EventHandler(bt_Click);
+        bt2.Click += new EventHandler(bt_Click);
+    }
+    public void bt_Click(Object sender, EventArgs e)
+    {
+        if(sender == bt1)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "텍스트 파일.txt";
+
+        }
+    }
+}
+
+
+
+
+
